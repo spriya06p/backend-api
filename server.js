5dc7087f356b25b559e30c999b8517ae9dd10b05
Himanix10 <himanix10@gmail.com>
2025-08-08 15:11:21 +0530

    added

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://Shanmugapriya:2006@cluster0.qrtn0jf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log(err));
+
+const itemRoutes = require('./routes/itemRoutes');
+app.use('/api/items', itemRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
